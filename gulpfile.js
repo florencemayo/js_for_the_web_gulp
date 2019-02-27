3700a5ace7ad89271bc970f5669cc8c4ff42bb79
Babu <babu@flos-MacBook-Pro.local>
2019-02-27 10:00:37 +0300

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const runSequence = require('run-sequence');
 
-gulp.task('copyAllFiles', () => {
+gulp.task('copyFiles', () => {
   
 
   return new Promise(function(resolve, reject) {
@@ -21,18 +21,50 @@ gulp.task('copyAllFiles', () => {
   });
 });
 
-gulp.task('checkCodeQuality', () => {
+
+gulp.task('lintJS', () => {
   return new Promise(function(resolve, reject) {
     gulp.src('*.js')
 	    .pipe(jshint({
-	        esversion: 6
+	        esversion: 8
 	    }))
 	    .pipe(jshint.reporter('default'))
-		.pipe(babel({
-	      presets: ['env']
-	    }))
+		.pipe(gulp.dest('dist'));
+
+    resolve();
+    });
+});
+
+
+gulp.task('minifyOldJS', () => {
+  return new Promise(function(resolve, reject) {
+    gulp.src('*.babel.js')
 	    .pipe(uglify())
 	    .pipe(gulp.dest('dist'));
+
+    resolve();
+    });
+});
+
+
+gulp.task('minifyCurrentJS', () => {
+  return new Promise(function(resolve, reject) {
+    gulp.src(['blog.js','functions.js'])
+	    .pipe(uglify())
+	    .pipe(gulp.dest('dist'));
+
+    resolve();
+    });
+});
+
+
+gulp.task('transpileJS', () => {
+  return new Promise(function(resolve, reject) {
+    gulp.src('*.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('dist'));
 
     resolve();
     });
@@ -49,7 +81,20 @@ gulp.task('babelPolyfill', () => {
  });
 
 
+gulp.task('watch', () => {
+  gulp.watch('*.js', ['lintJS', 'transpileJS']);
+  gulp.watch('*.babel.js', ['minifyOldJS']);
+  gulp.watch(['blog.js','functions.js'], ['minifyCurrentJS']);
+  gulp.watch(['*.html',
+	  	'*.json',
+	  	'README.md',
+	  	'.gitignore',
+	  	'./*node_modules/**/*',
+	  	'./*images/**/*'], ['copyFiles']);
+});
+
+
 gulp.task('default', (callback) => {
-  runSequence(['copyAllFiles', 'checkCodeQuality', 'babelPolyfill'], callback);
+  runSequence(['copyFiles', 'lintJS', 'minifyOldJS', 'minifyCurrentJS', 'transpileJS', 'babelPolyfill', 'watch'], callback);
 });
 
